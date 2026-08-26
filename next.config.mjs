c2c7f84cd326456f480e4001c91f79879adf8e67
BhaveshBasrani <bhaveshkumarbasrani@gmail.com>
2026-08-27 02:59:15 +0530

    chore: enforce static export configuration in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  ...(process.env.NEXT_EXPORT === 'true' ? { output: 'export' } : {}),
+  output: 'export',
   allowedDevOrigins: ['192.168.88.3:3000', '192.168.88.3', 'localhost:3000'],
   images: {
     unoptimized: true,
